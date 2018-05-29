a433211ff2bec7171bd0ca80bfe4bd64e23d441e
Jason Morse <jasonmo@Jasons-MacBook-Pro.local>
2018-05-28 23:25:36 -0700

    switching machines again

--- src/theming/ThemeCreation.ts
+++ src/theming/ThemeCreation.ts
@@ -1,74 +1,73 @@
-import { ITheme } from "./ITheme";
+import { ITheme, IThemeSettings } from "./ITheme";
 import { getTheme, hasTheme } from "./ThemeRegistry";
+import { ColorLayerType } from "./IColorLayerKey";
+import { IColor, getColorFromString } from "../coloring/color";
 
 
 /*
   generate a theme settings interface from an update string.  Possible options:
     theme: <name>                       - set the theme with the specified name as active
-    type: [theme|bg|switch]             - adjust the type of the default layer as specified
+    type: [themed|bg|switch]             - adjust the type of the default layer as specified
     deepen: number                      - adjust the current shade by the specified number of levels
     shade: number                       - set the current shade to the specified value
     fg|bg|accent: color                 - override the seed colors for the theme
 */
 export function themeFromChangeString(update: string, baseline: ITheme): ITheme {
-    const terms = update.split(' ');
+  const terms = update.split(' ');
+  const settings: Partial<IThemeSettings> = { };
 
-    for (let i: number = 0; i < terms.length; i++) {
-        switch (terms[i]) {
-            case 'theme:':
-                if (++i < terms.length && hasTheme(terms[i])) {
-                    return getTheme(terms[i]);
-                }
-                break;
-            case 'type:':
-                if (++i < terms.length) {
-                    
-                }
+  for (let i: number = 0; i < terms.length; i++) {
+    const cmd = terms[i];
+    switch (cmd) {
+      case 'theme:':
+        if (++i < terms.length && hasTheme(terms[i])) {
+            return getTheme(terms[i]);
         }
-    }
-}
-
-export function themeFromUpdateString(update: string, theme: ITheme): Partial<IThemeSettings>|undefined {
-    const terms = update.split(' ');
-    const offsets: IThemeOffsets = { ...theme.settings.offsets };
-    const def = offsets.default;
-    let didSomething: boolean = false;
-  
-    for (let i: number = 0; i < terms.length; i++) {
-      switch (terms[i]) {
-        case 'type:':
-          if (++i < terms.length) {
-            let changedType: boolean = true;
-            const param = terms[i];
-            if (param === 'theme' && def.type !== ColorLayerType.Accent) {
-              def.type = ColorLayerType.Accent;
-            } else if (param === 'bg' && def.type !== ColorLayerType.Bg) {
-              def.type = ColorLayerType.Bg;
+        break;
+      case 'type:':
+      case 'deepen:':
+      case 'shade:':
+        if (++i < terms.length) {
+          if (!settings.offsets) {
+            settings.offsets = { ...baseline.offsets };
+          }
+          const newDefault = { ...settings.offsets.default };
+          const param = terms[i];
+          if (cmd === 'type:') {
+            if (param === 'themed') {
+              newDefault.type = ColorLayerType.Accent;
+            } else if (param === 'bg') {
+              newDefault.type = ColorLayerType.Bg;
             } else if (param === 'switch') {
-              def.type = def.type === ColorLayerType.Accent ? ColorLayerType.Bg : ColorLayerType.Accent;
-            } else {
-              changedType = false;
+              newDefault.type = newDefault.type === ColorLayerType.Accent ? ColorLayerType.Bg : ColorLayerType.Accent;
             }
-            if (changedType) { 
-              didSomething = true;
-            }
-          }
-          break;
-        case 'deepen':
-        case 'shade':
-          const relative: boolean = (terms[i] === 'deepen');
-          if (++i < terms.length) {
-            const shade: number = parseInt(terms[i], 10);
-            if (!isNaN(shade) && (!relative || shade !== 0)) {
-              def.shade = relative ? def.shade + shade : shade;
-              didSomething = true;
+          } else {
+            const shade: number = parseInt(param, 10);
+            if (!isNaN(shade)) {
+              newDefault.shade = (cmd === 'deepen:') ? newDefault.shade + shade : shade;
             }
           }
-          break;
-      }
+          settings.offsets.default = newDefault;
+        }
+        break;
+      case 'fg:':
+      case 'bg:':
+      case 'accent:':
+        if (++i < terms.length) {
+          const newColor = terms[i];
+          if (!settings.seedColors) {
+            settings.seedColors = { ...baseline.seedColors };
+          }
+          const colors = settings.seedColors;
+          if (cmd === 'fg:') {
+            colors.fg = newColor;
+          } else if (cmd === 'bg:') {
+            colors.bg = newColor;
+          } else {
+            colors.accent = newColor;
+          }
+        }
+        break;
     }
-    if (didSomething) {
-      return { offsets } as Partial<IThemeSettings>;
-    }
-    return undefined;
   }
+}
